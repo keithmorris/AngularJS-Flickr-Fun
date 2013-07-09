80d231e82631d4b760928f9ed613ae50e5d7b281
Keith Morris <kmorris@brownbagmarketing.com>
2013-07-09 07:57:37 -0400

    made changes to use hg-resource for service calls

--- scripts/controllers/main-ctrl.js
+++ scripts/controllers/main-ctrl.js
@@ -1,82 +1,76 @@
-app.controller('MainCtrl',
-	[
-		'$scope', 'flickrRecent', '$location', '$route', '$routeParams','localStorageService',
-function ($scope, flickrRecent, $location, $route, $routeParams, store) {
+app.controller('MainCtrl', [
+	'$scope', 'flickrRecent', '$location', '$route', '$routeParams', 'localStorageService',
+	function ($scope, flickrRecent, $location, $route, $routeParams, store) {
 
-	$scope = $.extend($scope, {}, {
-		recentSearches: null,
-		init: function () {
-			$scope.setupWatchers();
-			$scope.recentSearches = store.get('recentSearches') || [];
-			$scope.$on('$routeChangeSuccess', this.onRouteChange);
-		},
-		setupWatchers: function () {
-			$scope.watchFlickrData();
-		},
-		watchFlickrData: function () {
-			$scope.$watch(function () {
-				return flickrRecent.photos;
-			}, function (data) {
-				$scope.photos = data;
-			}, true);
-		},
-		onSearchSubmit: function () {
-			$scope.updatePath('/search/' + $scope.searchQuery);
-		},
-		updatePath: function (path) {
-			$location.path(path);
-		},
-		addSearchHistoryItem: function (term) {
-			var searches = store.get('recentSearches') || [],
-				idx = $.inArray(term, searches);
-			if (idx !== -1) {
-				searches.splice(idx, 1);
+		$scope = $.extend($scope, {}, {
+			recentSearches: null,
+			init: function () {
+				$scope.setupWatchers();
+				$scope.recentSearches = store.get('recentSearches') || [];
+				$scope.$on('$routeChangeSuccess', this.onRouteChange);
+			},
+			setupWatchers: function () {
+				$scope.watchFlickrData();
+			},
+			watchFlickrData: function () {
+				$scope.$watch(function () {
+					return flickrRecent.photos;
+				}, function (data) {
+					$scope.photos = data;
+				}, true);
+			},
+			onSearchSubmit: function () {
+				$scope.updatePath('/search/' + $scope.searchQuery);
+			},
+			updatePath: function (path) {
+				$location.path(path);
+			},
+			addSearchHistoryItem: function (term) {
+				var searches = store.get('recentSearches') || [],
+					idx = $.inArray(term, searches);
+				if (idx !== -1) {
+					searches.splice(idx, 1);
+				}
+				searches.unshift(term);
+				if (searches.length > 10) {
+					searches = searches.slice(0, 10);
+				}
+				store.add('recentSearches', searches);
+				$scope.recentSearches = searches;
+			},
+			clearRecentSearches: function () {
+				$scope.recentSearches = [];
+				store.remove('recentSearches');
+				$scope.updatePath('/');
+			},
+			doSearch: function () {
+				var query = $routeParams.query;
+
+				$scope.addSearchHistoryItem(query);
+				$scope.query = $scope.searchQuery = query;
+				$scope.queryType = 'search';
+
+				flickrRecent.search($scope.query);
+			},
+			getRecent: function () {
+				$scope.query = $scope.searchQuery = '';
+				$scope.queryType = 'recent';
+				flickrRecent.getRecent();
+			},
+			getMore: function () {
+				flickrRecent.getMore();
+			},
+			onRouteChange: function () {
+				var action = $route.current.action;
+				switch (action) {
+					case 'recent':
+						$scope.getRecent();
+						break;
+					case 'search':
+						$scope.doSearch();
+						break;
+				}
 			}
-			searches.unshift(term);
-			if (searches.length > 10) {
-				searches = searches.slice(0, 10);
-			}
-			store.add('recentSearches', searches);
-			$scope.recentSearches = searches;
-		},
-		clearRecentSearches: function(){
-		    $scope.recentSearches = [];
-			store.remove('recentSearches');
-			$scope.updatePath('/');
-		},
-		doSearch: function () {
-			var query = $routeParams.query;
-
-			$scope.addSearchHistoryItem(query);
-
-			$scope.query = $scope.searchQuery = query;
-			$scope.queryType = 'search';
-			flickrRecent.search($scope.query);
-
-		},
-		getRecent: function () {
-			$scope.query = $scope.searchQuery = '';
-			$scope.queryType = 'recent';
-			flickrRecent.getRecent();
-		},
-		getMore: function () {
-			flickrRecent.getMore();
-		},
-		onRouteChange: function () {
-			var action = $route.current.action;
-			switch (action) {
-				case 'recent':
-					$scope.getRecent();
-					break;
-				case 'search':
-					$scope.doSearch();
-					break;
-			}
-		}
-
-	});
-
-	$scope.init();
-
-	window.scope = $scope;
-}]);
+		});
+		$scope.init();
+	}]);
--- scripts/services/flickr-recent.js
+++ scripts/services/flickr-recent.js
@@ -1,67 +1,68 @@
-app.factory('flickrRecent', ['$http', function ($http) {
+app.factory('flickrRecent', ['$http', '$resource', function ($http, $resource) {
 
-	var url = 'http://api.flickr.com/services/rest/',
-		methods = {
-			'recent': 'flickr.photos.getRecent',
-			'search': 'flickr.photos.search'
-		},
-		apiKey = '51024b980a33aded7b162d9074b9465c',
-		jsonCallback = 'JSON_CALLBACK',
-		extras = ["url_q"],
-		append = false,
-		currentCall = '',
+	var flickr = $resource('http://api.flickr.com/services/rest/',
+			{
+				api_key: '51024b980a33aded7b162d9074b9465c',
+				extras: 'url_q',
+				//description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o
+				jsoncallback: 'JSON_CALLBACK',
+				format: 'json',
+				per_page: 100
+			},
+			{
+				recent: {
+					method: 'JSONP',
+					params: {
+						method: 'flickr.photos.getRecent'
+					}
+				},
+				search: {
+					method: 'JSONP',
+					params: {
+						method: 'flickr.photos.search'
+					}
+				}
+			}),
+
+		currentCall,
 		totalPages,
-		currentPage;
+		currentPage,
+		append = false,
 
-	function getSearchURL(query) {
-		return currentCall = url + '?method=' + methods.search +
-			'&text=' + query +
-			'&extras=' + extras.join(',') +
-			'&api_key=' + apiKey +
-			'&format=json&' +
-			'jsoncallback=' + jsonCallback;
-	}
+		svc = {
+			photos: [],
+			dataLoading: false,
+			getRecent: function () {
+				append = false;
+				svc.makeCall('recent');
+			},
+			search: function (query) {
+				append = false;
+				svc.makeCall('search', {text: query});
+			},
+			makeCall: function (method, params) {
+				params = params || {};
+				currentCall = {
+					method: method,
+					params: params
+				};
+				flickr[method](params, this.onLoadSuccess);
+			},
+			getMore: function () {
+				if (svc.dataLoading) return
 
-	function getRecentURL() {
-		return currentCall = url + '?method=' + methods.recent +
-			'&extras=' + extras.join(',') +
-			'&api_key=' + apiKey +
-			'&format=json' +
-			'&jsoncallback=' + jsonCallback
-	}
-
-	svc = {
-		photos: [],
-		dataLoading: false,
-		getRecent: function () {
-			append = false;
-			svc.makeApiCall(getRecentURL());
-		},
-		search: function (query) {
-			append = false;
-			svc.makeApiCall(getSearchURL(query));
-		},
-		getMore: function () {
-			append = true;
-			currentPage++;
-			svc.makeApiCall(currentCall + '&page=' + currentPage);
-		},
-		makeApiCall: function (apiCall) {
-			if (svc.dataLoading) return;
-			svc.dataLoading = true;
-			$http.jsonp(apiCall)
-				.success(svc.onLoadSuccess)
-				.error(function () {
-					console.log('error', arguments);
-					svc.dataLoading = false;
-				});
-		},
-		onLoadSuccess: function (data) {
-			currentPage = data.photos.page;
-			totalPages = data.photos.pages;
-			svc.photos = (append) ? svc.photos.concat(data.photos.photo) : data.photos.photo;
-			svc.dataLoading = false;
-		}
-	};
+				svc.dataLoading = true;
+				append = true;
+				currentPage++;
+				currentCall.params.page = currentPage;
+				svc.makeCall(currentCall.method, currentCall.params);
+			},
+			onLoadSuccess: function (data) {
+				currentPage = data.photos.page;
+				totalPages = data.photos.pages;
+				svc.photos = (append) ? svc.photos.concat(data.photos.photo) : data.photos.photo;
+				svc.dataLoading = false;
+			}
+		};
 	return svc;
 }]);
